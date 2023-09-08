d28d3ed4ea0fdf848f4ab8aaa0306cb7c049b3ea
Gavin Schulze <118791618+gbschulze@users.noreply.github.com>
2023-09-08 15:34:58 -0400

    Update git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Gavin Schulze
+ *        Date: September 8, 2023
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
